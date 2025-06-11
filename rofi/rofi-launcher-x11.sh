339e1dcc77331ade79893c223fea5df7970d3693
Tohid Eghdami <TohidEq@gmail.com>
2025-06-12 03:02:35 +0330

    Rofi

---
+++ rofi/rofi-launcher-x11.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+## Copyright (C) 2020-2024 Aditya Shakya <adi1090x@gmail.com>
+
+
+
+# Import Current Theme
+STYLE="rofi-blur"
+RASI="$STYLE/launcher.rasi"
+
+# Run
+rofi \
+    -x11 \
+    -show drun \
+    -theme ${RASI}
