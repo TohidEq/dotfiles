f2175d72c8a394ef8fa0cb945efa8fb44ac07fe1
Tohid Eghdami <TohidEq@gmail.com>
2025-12-26 11:54:33 +0330

    🥭

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -11,17 +11,17 @@ set __fish_git_prompt_showstashstate yes
 set __fish_git_prompt_showuntrackedfiles yes
 set __fish_git_prompt_showupstream yes
 # Status Chars
-set __fish_git_prompt_char_dirtystate '⠀ '
-set __fish_git_prompt_char_stagedstate '⠀ '
-set __fish_git_prompt_char_untrackedfiles '⠀ '
-set __fish_git_prompt_char_stashstate '⠀ '
-set __fish_git_prompt_char_upstream_ahead '⠀ '
-set __fish_git_prompt_char_upstream_behind '⠀ '
+set __fish_git_prompt_char_dirtystate ' '
+set __fish_git_prompt_char_stagedstate ' '
+set __fish_git_prompt_char_untrackedfiles ' '
+set __fish_git_prompt_char_stashstate ' '
+set __fish_git_prompt_char_upstream_ahead ' '
+set __fish_git_prompt_char_upstream_behind ' '
 
 set __fish_git_prompt_char_invalidstate '⠀✖ '
 set __fish_git_prompt_char_cleanstate '⠀✔ '
 
-set __fish_git_prompt_char_upstream_diverged '⠀  '
+set __fish_git_prompt_char_upstream_diverged '  '
 
 set __fish_git_prompt_char_upstream_equal ' '
 # Enable color hints
