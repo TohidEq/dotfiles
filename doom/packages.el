c852efb4ba23ecc607f1c23d1a1fac65f595c83b
Tohid Eghdami <TohidEq@gmail.com>
2026-09-20 04:04:32 +0330

    update doom: install yuck-mode

--- doom/packages.el
+++ doom/packages.el
@@ -52,7 +52,7 @@
 ;; (unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;; (unpin! t)
- 
+
 ;; Add gruvbox theme
 (package! gruvbox-theme)
 
@@ -99,7 +99,7 @@
 
 
 
-
+(package! yuck-mode)
 
 
 
